b557cf7d90b433237bc58c6b595949613fd1c2f2
sotomaque <sotomaque@gmail.com>
2019-12-27 10:11:11 -0800

    first commit

---
+++ screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
+
+
+export default function Home() {
+    return (
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.titleText}>Home Screen</Text>
+        </View>
+    )
+}
+
