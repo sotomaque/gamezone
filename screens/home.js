51edd7fba2b3ee87687bc03f3866ea3537508ab8
sotomaque <sotomaque@gmail.com>
2019-12-27 10:41:43 -0800

    added navigation between home screen and review screen, passing in props

--- screens/home.js
+++ screens/home.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+    const [reviews, setReviews] = useState([
+        { title: 'Star Wars', rating: 5, body: 'lorem ipsum', key: '1'},
+        { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2'},
+        { title: 'Not so Final', rating: 3, body: 'lorem ipsum', key: '3'}
+    ]);
+
+
+
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
+            <FlatList 
+                data={reviews}
+                renderItem={({item}) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -3,13 +3,13 @@ import { View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>
-                Review Details Screen
-            </Text>
+            <Text>{navigation.getParam('title')}</Text>
+            <Text>{navigation.getParam('body')}</Text>
+            <Text>{navigation.getParam('rating')}</Text>
         </View>
     )
 }
-
